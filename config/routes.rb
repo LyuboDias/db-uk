c2f972d75bda19264721fbcfb143a2155eb2a4ac
Lyubomir Dias <lyubomirdias@gmail.com>
2020-10-18 17:50:11 +0100

    changed music_path

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :pages do
-    collection do
-      get :music
-    end
-  end
+
+  get '/music' => 'pages#music'
 end
