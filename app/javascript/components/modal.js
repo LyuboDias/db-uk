c97182a0c55671009de11dbd706d6e7b1dc245d8
Lyubomir Dias <lyubomirdias@gmail.com>
2021-01-14 16:39:22 +0000

    added terms and privacy pages

--- app/javascript/components/modal.js
+++ app/javascript/components/modal.js
@@ -1,22 +1,26 @@
-let modal = document.getElementById('simpleModal');
-let modalBtn = document.getElementById('modalBtn');
-let modalBtn2 = document.getElementById('modalBtn2');
-let closeBtn = document.getElementById('closeBtn');
+// --------resume modal---------
+let modal = document.getElementById("simpleModal");
+let modalBtn = document.getElementById("modalBtn");
+let modalBtn2 = document.getElementById("modalBtn2");
+let closeBtn = document.getElementById("closeBtn");
 
-modalBtn.addEventListener('click', openModal);
-modalBtn2.addEventListener('click', openModal);
-closeBtn.addEventListener('click', closeModal);
-window.addEventListener('click', closeOutside);
+modalBtn.addEventListener("click", openModal);
+modalBtn2.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+window.addEventListener("click", closeOutside);
 
-
-function openModal(){
-  modal.style.display = 'block';
+function openModal() {
+  modal.style.display = "block";
 }
-function closeModal(){
-  modal.style.display = 'none';
+function closeModal() {
+  modal.style.display = "none";
 }
-function closeOutside(e){
+function closeOutside(e) {
   if (e.target == modal) {
-    modal.style.display = 'none';
+    modal.style.display = "none";
   }
 }
+// ----coookie modal-----
+$(document).ready(function () {
+  $("#cookieModal").modal("show");
+});
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  get '/terms-of-use' => 'pages#terms'
+  get '/privacy-policy' => 'pages#policy'
   get '/music' => 'pages#music'
 end
