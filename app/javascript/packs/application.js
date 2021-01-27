9ed568fbd233c65040be2269130975ea7e87da0a
Lyubomir Dias <lyubomirdias@gmail.com>
2021-01-27 15:17:19 +0000

    updated Dr Sen Link

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,7 +25,7 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-import "../components/modal.js";
+import "../components/modal";
 
 document.addEventListener("turbolinks:load", () => {
   // Call your functions here, e.g:
